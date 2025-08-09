0626e13c90b57fae1fa195973c88bc805b0e45b0
Rodolfo Venero Noriega <rveneron@gmail.com>
2025-08-09 19:34:08 -0400

    [feat]: Update footer

---
+++ components/core/trans/trans.component.tsx
@@ -0,0 +1,32 @@
+import { memo } from 'react';
+import { Trans as ReactTrans } from 'react-i18next';
+import { ClassNameProps } from '@/types/classnames-props.type';
+
+const defaultComponents = {
+  primary: <span className="text-primary" />,
+  secondary: <span className="text-secondary" />,
+  error: <span className="text-red-600" />,
+  important: <span className="font-bold" />,
+  italic: <span className="italic" />,
+  br: <br />
+};
+
+export type Props = ClassNameProps & {
+  message: string;
+  values?: Record<string, string>;
+  components?: Record<string, any>;
+};
+
+const Trans = ({ className, message, values = {}, components = {} }: Props) => {
+  return (
+    <span className={className}>
+      <ReactTrans
+        i18nKey={message}
+        components={{ ...defaultComponents, ...components }}
+        values={values}
+      />
+    </span>
+  );
+};
+
+export default memo(Trans);
--- modules/common/components/footer/footer.component.tsx
+++ modules/common/components/footer/footer.component.tsx
@@ -8,6 +8,7 @@ import FooterContactCard from '@/modules/common/components/footer-contact-card/f
 import { CustomInput } from '@/components/ui/custom-input';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = ClassNameProps & TProps;
 
@@ -20,21 +21,27 @@ const Footer = ({ className, t }: Props) => {
             <div className="subscription-title">{t('menu:footer.subscription.title')}</div>
             <div className="subscription-subtitle">{t('menu:footer.subscription.subtitle')}</div>
           </div>
-          <CustomInput
-            type={'email'}
-            className={'email-input'}
-            placeholder={t('common:email')}
-            startAdornment={
-              <Image
-                src={'/images/footer/subscription-email.webp'}
-                alt={'email'}
-                width={20}
-                height={20}
-                className={'ml-4 h-[20px] w-[20px]'}
-              />
-            }
-            endAdornment={<Button className={'send-button'}>{t('common:send')}</Button>}
-          />
+          <div className={'max-w-[420px]'}>
+            <CustomInput
+              type={'email'}
+              className={'email-input'}
+              placeholder={t('common:email')}
+              startAdornment={
+                <Image
+                  src={'/images/footer/subscription-email.webp'}
+                  alt={'email'}
+                  width={20}
+                  height={20}
+                  className={'ml-4 h-[20px] w-[20px]'}
+                />
+              }
+              endAdornment={<Button className={'send-button'}>{t('common:send')}</Button>}
+            />
+            <span className={'terms'}>
+              {t('menu:footer.termsAndPolicies.part1')}
+              <Link href={'/terms'}>{t('menu:footer.termsAndPolicies.part2')}</Link>
+            </span>
+          </div>
         </div>
         <div className="contacts">
           <FooterContactCard
