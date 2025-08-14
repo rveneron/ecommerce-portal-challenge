dcaa38587fb8e3155d6ba99f164c53ed4d6e882d
Rodolfo Venero Noriega <rveneron@gmail.com>
2025-08-13 23:33:43 -0400

    optimizations

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  extends: ['next/core-web-vitals', 'next/typescript'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off'
   },
