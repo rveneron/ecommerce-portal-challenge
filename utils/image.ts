6e2ba3be75cd1d8a598002888ca424ba23f06821
Rodolfo Venero Noriega <rveneron@gmail.com>
2025-08-16 19:50:29 -0400

    [feat]: Add categories and products details pages

--- utils/image.ts
+++ utils/image.ts
@@ -2,5 +2,6 @@ import { IMediaItem } from '@/interfaces/media.interface';
 import { PUBLIC_APP_URL } from '@/constants';
 
 export const getImageAbsolutePath = (image: IMediaItem) => {
-  return `${PUBLIC_APP_URL}/${image.url}`.replace('//', '/');
+  return image.url;
+  // return `${PUBLIC_APP_URL}/${image.url}`.replace('//', '/');
 };
